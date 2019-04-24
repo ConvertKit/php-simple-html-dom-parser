2133982fa268436eca5a8ea2a2dd47344736edb0
Travis Northcutt <travis@travisnorthcutt.com>
2019-04-24 14:12:57 -0600

    Safely define constants
    Related: https://github.com/ConvertKit/ConvertKit-WordPress/issues/200
    
    Prior to this change, constant definitions were not wrapped in safety checks
    of `if ( !defined() )`. Because of this, two or more plugins using this library
    on the same WordPress site resulted in PHP notices being thrown.
    
    This change wraps constant definitions to avoid this problem.

--- src/KubAT/PhpSimple/lib/simple_html_dom.php
+++ src/KubAT/PhpSimple/lib/simple_html_dom.php
@@ -63,38 +63,82 @@ namespace simple_html_dom;
  * All of the Defines for the classes below.
  * @author S.C. Chen <me578022@gmail.com>
  */
-define('HDOM_TYPE_ELEMENT', 1);
-define('HDOM_TYPE_COMMENT', 2);
-define('HDOM_TYPE_TEXT', 3);
-define('HDOM_TYPE_ENDTAG', 4);
-define('HDOM_TYPE_ROOT', 5);
-define('HDOM_TYPE_UNKNOWN', 6);
-define('HDOM_QUOTE_DOUBLE', 0);
-define('HDOM_QUOTE_SINGLE', 1);
-define('HDOM_QUOTE_NO', 3);
-define('HDOM_INFO_BEGIN', 0);
-define('HDOM_INFO_END', 1);
-define('HDOM_INFO_QUOTE', 2);
-define('HDOM_INFO_SPACE', 3);
-define('HDOM_INFO_TEXT', 4);
-define('HDOM_INFO_INNER', 5);
-define('HDOM_INFO_OUTER', 6);
-define('HDOM_INFO_ENDSPACE', 7);
+if ( ! defined( 'HDOM_TYPE_ELEMENT' ) ) {
+	define( 'HDOM_TYPE_ELEMENT', 1 );
+}
+if ( ! defined( 'HDOM_TYPE_COMMENT' ) ) {
+	define( 'HDOM_TYPE_COMMENT', 2 );
+}
+if ( ! defined( 'HDOM_TYPE_TEXT' ) ) {
+	define( 'HDOM_TYPE_TEXT', 3 );
+}
+if ( ! defined( 'HDOM_TYPE_ENDTAG' ) ) {
+	define( 'HDOM_TYPE_ENDTAG', 4 );
+}
+if ( ! defined( 'HDOM_TYPE_ROOT' ) ) {
+	define( 'HDOM_TYPE_ROOT', 5 );
+}
+if ( ! defined( 'HDOM_TYPE_UNKNOWN' ) ) {
+	define( 'HDOM_TYPE_UNKNOWN', 6 );
+}
+if ( ! defined( 'HDOM_QUOTE_DOUBLE' ) ) {
+	define( 'HDOM_QUOTE_DOUBLE', 0 );
+}
+if ( ! defined( 'HDOM_QUOTE_SINGLE' ) ) {
+	define( 'HDOM_QUOTE_SINGLE', 1 );
+}
+if ( ! defined( 'HDOM_QUOTE_NO' ) ) {
+	define( 'HDOM_QUOTE_NO', 3 );
+}
+if ( ! defined( 'HDOM_INFO_BEGIN' ) ) {
+	define( 'HDOM_INFO_BEGIN', 0 );
+}
+if ( ! defined( 'HDOM_INFO_END' ) ) {
+	define( 'HDOM_INFO_END', 1 );
+}
+if ( ! defined( 'HDOM_INFO_QUOTE' ) ) {
+	define( 'HDOM_INFO_QUOTE', 2 );
+}
+if ( ! defined( 'HDOM_INFO_SPACE' ) ) {
+	define( 'HDOM_INFO_SPACE', 3 );
+}
+if ( ! defined( 'HDOM_INFO_TEXT' ) ) {
+	define( 'HDOM_INFO_TEXT', 4 );
+}
+if ( ! defined( 'HDOM_INFO_INNER' ) ) {
+	define( 'HDOM_INFO_INNER', 5 );
+}
+if ( ! defined( 'HDOM_INFO_OUTER' ) ) {
+	define( 'HDOM_INFO_OUTER', 6 );
+}
+if ( ! defined( 'HDOM_INFO_ENDSPACE' ) ) {
+	define( 'HDOM_INFO_ENDSPACE', 7 );
+}
 
 /** The default target charset */
-defined('DEFAULT_TARGET_CHARSET') || define('DEFAULT_TARGET_CHARSET', 'UTF-8');
+if ( ! defined( 'DEFAULT_TARGET_CHARSET' ) ) {
+	define( 'DEFAULT_TARGET_CHARSET', 'UTF-8' );
+}
 
 /** The default <br> text used instead of <br> tags when returning text */
-defined('DEFAULT_BR_TEXT') || define('DEFAULT_BR_TEXT', "\r\n");
+if ( ! defined( 'DEFAULT_BR_TEXT' ) ) {
+	define( 'DEFAULT_BR_TEXT', "\r\n" );
+}
 
 /** The default <span> text used instead of <span> tags when returning text */
-defined('DEFAULT_SPAN_TEXT') || define('DEFAULT_SPAN_TEXT', ' ');
+if ( ! defined( 'DEFAULT_SPAN_TEXT' ) ) {
+	define( 'DEFAULT_SPAN_TEXT', ' ' );
+}
 
 /** The maximum file size the parser should load */
-defined('MAX_FILE_SIZE') || define('MAX_FILE_SIZE', 600000);
+if ( ! defined( 'MAX_FILE_SIZE' ) ) {
+	define( 'MAX_FILE_SIZE', 600000 );
+}
 
 /** Contents between curly braces "{" and "}" are interpreted as text */
-define('HDOM_SMARTY_AS_TEXT', 1);
+if ( ! defined( 'HDOM_SMARTY_AS_TEXT' ) ) {
+	define( 'HDOM_SMARTY_AS_TEXT', 1 );
+}
 
 // helper functions
 // -----------------------------------------------------------------------------
